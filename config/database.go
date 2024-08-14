08b5ca4c4b7581d303813e565b9ea858efebbac7
payahbro <payah45678@gmail.com>
2024-08-14 17:36:11 +0700

    Update database.go, add .env_sample, and update .gitignore to exclude .env

--- config/database.go
+++ config/database.go
@@ -2,6 +2,10 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 	"time"
 )
 
@@ -9,7 +13,28 @@ func OpenDB() *sql.DB {
 	var db *sql.DB
 	var err error
 
-	db, err = sql.Open("mysql", "root:admin@tcp(localhost:3306)/todo?parseTime=true")
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbOptions := os.Getenv("DB_OPTIONS")
+
+	// Check if environment variables are set
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		log.Println("One or more environment variables are not set")
+		return nil
+	}
+
+	// Create the data source name (DSN)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbOptions)
+
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
